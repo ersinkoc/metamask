46dd57d60f612aef58e77033ad74d6bcd87ad566
Anthony Burak DURSUN <badursun@gmail.com>
2022-01-30 15:05:15 +0300

    x
    x

--- dist/js/app.js
+++ dist/js/app.js
@@ -51,6 +51,7 @@ $(document).ready(function() {
             CurrentSymbol   : 'MONEY',
             TokenUSDPrice   : 0.025,
             NetworkInfo     : [],
+            tokenDecimal    : 8,
             LastBlockNumber : null
         },
         /* ########################
